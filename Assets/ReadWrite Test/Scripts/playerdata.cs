14b9e27d5afeebf286ca1b52bec4d642581ad6b6
DavidCM <dcm.robin@gmail.com>
2021-11-23 19:27:02 +0000

    made a button that writes the playerdata to data.txt

--- Assets/ReadWrite Test/Scripts/playerdata.cs
+++ Assets/ReadWrite Test/Scripts/playerdata.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
+using System.IO;
 
 public class playerdata : MonoBehaviour
 {
@@ -14,15 +16,20 @@ public class playerdata : MonoBehaviour
     }
     statsData myObject = new statsData();
     string json;
+    string path;
+    string content;
     void Start()
     {
+        path = Application.dataPath + "/ReadWrite Test/data.txt";
         myObject.level = 1;
         myObject.timeElapsed = 47.5f;
         myObject.playerName = "Dr Charles Francis";
-    }
-    public void toJSON()
-    {
         json = JsonUtility.ToJson(myObject);
-        Debug.Log(json);
+        content = json;
+    }
+    public void CreateJSONCode()
+    {
+        File.Delete(path);
+        File.AppendAllText(path, content + "\n");
     }
 }
