4715f23cca35ab0260b09ad64435ba2aada860ad
DavidCM <dcm.robin@gmail.com>
2021-11-26 11:50:06 +0000

    phew. now when you change data in data.txt then run the game, the data of the player gets changed, according to what's in data.txt.

--- Assets/ReadWrite Test/Scripts/playerdata.cs
+++ Assets/ReadWrite Test/Scripts/playerdata.cs
@@ -14,22 +14,35 @@ public class playerdata : MonoBehaviour
         public float timeElapsed;
         public string playerName;
     }
-    statsData myObject = new statsData();
+    statsData myObject;
     string json;
     string path;
     string content;
     void Start()
     {
+        //Creates a new instance of the class...
+        myObject = new statsData();
+
+        //Initializes the data...
         path = Application.dataPath + "/ReadWrite Test/data.txt";
         myObject.level = 1;
         myObject.timeElapsed = 47.5f;
         myObject.playerName = "Dr Charles Francis";
-        json = JsonUtility.ToJson(myObject);
-        content = json;
-    }
-    public void CreateJSONCode()
-    {
-        File.Delete(path);
-        File.AppendAllText(path, content + "\n");
+
+        //Converts the data to Json...
+        json = JsonUtility.ToJson(myObject, true);
+
+        //Uncomment these if you want to rewrite the data to data.txt
+        //File.Delete(path);
+        //File.WriteAllText(path, content);
+
+        //Reads the contents of data.txt...
+        json = File.ReadAllText(path);
+
+        //Overwrites the data in the myObject class with new data...
+        JsonUtility.FromJsonOverwrite(json, myObject);
+
+        //And finally prints the data values of myObject.
+        Debug.Log(json);
     }
 }
